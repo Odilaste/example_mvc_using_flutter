aa53436734cd59c653a138c8d1c9e25d1946288c
Odilaste <odilontshiyire@gmail.com>
2022-11-21 06:48:36 +0200

    Mis a jour du lundi 20 Novembre 3

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             const Text(
-              'Thanks  Dear Developer',
+              'Thanks my Dear Developer',
             ),
             Text(
               '$_counter',
